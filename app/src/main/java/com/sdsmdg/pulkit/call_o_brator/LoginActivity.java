89650959dda860d7f6f26faa8da7ef069a691b29
pkarira <pulkit5208@gmail.com>
2017-10-16 23:30:21 +0530

    added android 6 permissions

--- app/src/main/java/com/sdsmdg/pulkit/call_o_brator/LoginActivity.java
+++ app/src/main/java/com/sdsmdg/pulkit/call_o_brator/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.sdsmdg.pulkit.call_o_brator;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +14,10 @@ import android.widget.EditText;
 public class LoginActivity extends AppCompatActivity {
     public static String contactNumber;
     public static boolean loggedIn=false;
+    public final static int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 11;
+    public final static int MY_PERMISSIONS_REQUEST_READ_PHONE_CONTACTS = 12;
+    public final static int MY_PERMISSIONS_WAKE_LOCK = 13;
+    public final static int MY_PERMISSIONS_KEYGUARD = 14;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,5 +38,90 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_PHONE_STATE},
+                        MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
+            }
+        }
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_CONTACTS},
+                        MY_PERMISSIONS_REQUEST_READ_PHONE_CONTACTS);
+            }
+        }
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_CONTACTS},
+                        MY_PERMISSIONS_REQUEST_READ_PHONE_CONTACTS);
+            }
+        }
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.WAKE_LOCK)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WAKE_LOCK},
+                        MY_PERMISSIONS_WAKE_LOCK);
+            }
+        }
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.DISABLE_KEYGUARD)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.DISABLE_KEYGUARD},
+                        MY_PERMISSIONS_KEYGUARD);
+            }
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_PHONE_STATE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                }
+                return;
+            }
+            case MY_PERMISSIONS_REQUEST_READ_PHONE_CONTACTS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                }
+                return;
+            }
+            case MY_PERMISSIONS_KEYGUARD: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                }
+                return;
+            }
+            case MY_PERMISSIONS_WAKE_LOCK:{
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                }
+                return;
+            }
+        }
     }
 }
